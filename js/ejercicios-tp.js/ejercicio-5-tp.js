7c8ab4346ee39c5c1569467fcc3f2ca46fd0751f
Javier Jimenez <javier.sniguel@gmail.com>
2023-10-26 21:44:04 -0300

    Se resolvio el ejercicio 5 del tp y se agregaron estilos a la pgina

---
+++ js/ejercicios-tp.js/ejercicio-5-tp.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+document.write("El resultado es = " + (numero1 + numero2));
