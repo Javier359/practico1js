2f30b69ea686a713bd19f0a7aea35f40b8427ad5
Javier Jimenez <javier.sniguel@gmail.com>
2023-10-26 22:17:06 -0300

    Se resolvio el ejercicio 8 del tp y se agregaron estilos a la pagina

---
+++ js/ejercicios-tp.js/ejercicio-8-tp.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+if (numero1%2==0) {
+    document.write("El numero es divisible entre 2")
+}else{
+    document.write("El numero no es divisible entre 2");
+}
+
