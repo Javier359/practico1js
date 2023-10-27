7b259f59263545b6a157be06f40c4d5c67f5e058
Javier Jimenez <javier.sniguel@gmail.com>
2023-10-26 23:20:53 -0300

    Se resolvio el ejercicio 10 del tp y se agregaron estilos a la pagina

---
+++ js/ejercicios-tp.js/ejercicio-10-tp.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Ingrese un numero"));
+if( numero % 2 == 0){ document.write("El numero " + numero + " Es divisible por 2")
+} else if(numero % 3 == 0){document.write("El numero "+numero+" Es divisible por 3")
+} else if(numero % 5 == 0){document.write("El numero "+numero+" Es divisible por 5")
+}else if(numero % 7 == 0){document.write("El numero "+numero+" Es divisible por 7")
+} else{document.write("El numero "+numero+" No es divisble por ninguno de los numeros")}
+
